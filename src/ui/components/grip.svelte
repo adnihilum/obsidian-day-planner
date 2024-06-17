2d3897d724a2dbcbee9e436e3b3f0f6996a362af
adnihilum <b1er.new@gmail.com>
2024-06-18 02:36:58 +0300

    fix: "vanishing grip" bug, also make it visible only on hover

--- src/ui/components/grip.svelte
+++ src/ui/components/grip.svelte
@@ -10,16 +10,20 @@
 
 <style>
   .grip {
-    position: relative;
-    right: -4px;
+    position: absolute;
+    z-index: 10;
+    right: 0;
 
     grid-column: 2;
     align-self: flex-start;
 
     color: var(--text-faint);
+
+    opacity: 0;
   }
 
   .grip:hover {
     color: var(--text-muted);
+    opacity: 1;
   }
 </style>
