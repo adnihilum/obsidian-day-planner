e1a2f8869bba33486c43af2113ac7a43c55f4847
adnihilum <b1er.new@gmail.com>
2024-06-23 15:54:23 +0300

    fix: untrliable resize handle

--- src/ui/components/resize-handle.svelte
+++ src/ui/components/resize-handle.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
   export let visible = true;
+  $: cssVarStyles = visible
+    ? `--resize-handle-hover-color:var(--divider-color-hover)`
+    : `--resize-handle-hover-color:transparent`;
 </script>
 
 <hr
-  style:display={visible ? "block" : "none"}
   class="workspace-leaf-resize-handle"
+  style={cssVarStyles}
   on:mousedown|stopPropagation
 />
 
 <style>
   :not(#dummy).workspace-leaf-resize-handle {
     cursor: row-resize;
-
+    display: block;
     right: 0;
     bottom: 0;
     left: 0;
@@ -19,5 +22,10 @@
     height: calc(var(--divider-width-hover) * 2);
 
     border-bottom-width: var(--divider-width);
+    background-color: transparent;
+  }
+
+  :not(#dummy).workspace-leaf-resize-handle:hover {
+    background-color: var(--resize-handle-hover-color);
   }
 </style>
